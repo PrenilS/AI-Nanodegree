e34f201e7763d76e8c0114a3814b13bd2c5f71cd
Prenil Sewmohan <prenils@lightstone.local>
2019-02-07 16:06:45 +0200

    FEAT: add alpha beta iterative depth search player to sameple_players

--- 3_adversarial search/sample_players.py
+++ 3_adversarial search/sample_players.py
@@ -133,3 +133,61 @@ class MinimaxPlayer(BasePlayer):
         own_liberties = state.liberties(own_loc)
         opp_liberties = state.liberties(opp_loc)
         return len(own_liberties) - len(opp_liberties)
+
+class Alpha_beta_player(BasePlayer):
+
+    def get_action(self, state):
+        import random
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            self.queue.put(self.alpha_beta_search(state, depth=3))
+
+    def alpha_beta_search(self, state, depth):
+
+        def min_value(state, depth, alpha, beta):
+            if state.terminal_test():
+                return state.utility(self.player_id)
+            if depth <= 0:
+                return self.score(state)
+
+            v = float("inf")
+
+            for a in state.actions():
+                v = min(v, max_value(state.result(a), depth-1, alpha, beta))
+                if v <= alpha:
+                    return v
+                beta = min(beta, v)
+            return v
+
+        def max_value(state, depth, alpha, beta):
+            if state.terminal_test():
+                return state.utility(self.player_id)
+            if depth <= 0:
+                return self.score(state)
+            v = float("-inf")
+            for a in state.actions():
+                v = max(v, min_value(state.result(a), depth-1, alpha, beta))
+                if v >= beta:
+                    return v
+                alpha = max(alpha, v)
+            return v
+
+        alpha = float("-inf")
+        beta = float("inf")
+        best_score = float("-inf")
+        best_move = None
+        for a in state.actions():
+            v = min_value(state.result(a), depth-1, alpha, beta)
+            alpha = max(alpha, v)
+            if v >= best_score:
+                best_score = v
+                best_move = a
+        return best_move
+
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        return len(own_liberties) - len(opp_liberties)
